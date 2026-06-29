ada6acfa9e13d533ff8fe6c7223c0c90a5cd506a
Queen Cadlax <qcadlax@gmail.com>
2026-06-30 00:54:48 +0200

    updated meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,33 +47,11 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      {
-        url: '/favicon_io/favicon-32x32.png',
-        type: 'image/png',
-      },
-      {
-        url: '/favicon_io/favicon-16x16.png',
-        type: 'image/png',
-      },
-      {
-        url: '/favicon_io/apple-touch-icon.png',
-        rel: 'apple-touch-icon',
-      },
-      {
-        url: '/favicon_io/android-chrome-192x192.png',
-        type: 'image/png',
-      },
-      {
-        url: '/favicon_io/android-chrome-512x512.png',
-        type: 'image/png',
-      },
-      {
-        url: '/favicon_io/favicon.ico',
-        type: 'image/x-icon',
-      },
+      { url: '/favicon.ico' },
+      { url: '/icon.png', type: 'image/png' },
     ],
-    shortcut: '/favicon_io/favicon-32x32.png',
-    apple: '/favicon_io/apple-touch-icon.png',
+    apple: '/apple-icon.png',
+    shortcut: '/favicon.ico',
   },
   manifest: '/favicon_io/site.webmanifest',
   other: {
